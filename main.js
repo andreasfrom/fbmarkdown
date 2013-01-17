ca83b7f3d77820f493d4e0801b48068db91df898
Andreas From <andro.from@gmail.com>
2013-01-17 22:00:05 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,95 @@
+// fbmarkdown by Andreas From
+
+var converter = Markdown.getSanitizingConverter();
+var smileys = false;
+
+RegExp.quote = function(str) {
+     return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+ };
+
+converter.hooks.chain("preConversion", function(text) {
+    text = text.replace(/(<(br)([^>]+|)>)/ig, "\n\n"); // br to \n\n
+    tmp = document.createElement("p");
+    tmp.innerHTML = text;
+    text = tmp.innerText;
+    text = text.replace(/(<([^>]+|)>)/ig, ""); // strip all tags
+    text = text.replace(/(\n\s+\#)/g, "\n#"); // strip space before # when first character on line
+    return text;
+});
+
+converter.hooks.chain("postConversion", function(text) {
+    if (smileys) {
+        for (var i = 0; i < smileys.length; i+=2) {
+            var rep = smileys[i].innerText;
+            var re = new RegExp("(^|[\\s])" + RegExp.quote(rep), "gi");
+            text = text.replace(re, " " + smileys[i].outerHTML + smileys[i+1].outerHTML);
+        }
+    }
+    smileys = false;
+    text = text.replace(/(^<p>)/, "<span>");
+    text = text.replace(/(<\/p>$)/, "</span>");
+    return text;
+});
+
+function waitForPause(ms, callback) {
+    var timer;
+
+    return function() {
+        var self = this, args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            callback.apply(self, args);
+        }, ms);
+    };
+}
+
+function unfoldUpdates() {
+    $(".text_exposed_link").each(function() {
+        $($(this).children()[0]).click();
+    });
+    $(".text_exposed_hide").remove();
+}
+
+function handleElements(selector) {
+    $(selector).not(".fbmarkdown").each(function() {
+        text = this.innerHTML;
+        smileys = $(this).find("span.emoticon_text, span.emoticon");
+        html = converter.makeHtml(text);
+        $(this).html(html);
+        $(this).find("code").each(function() {
+            this.innerText = this.innerText.replace(/\n\n/g, "\n");
+            hljs.highlightBlock(this);
+            $(this).addClass("fbmdcss");
+        });
+        $(this).find('*').each(function() {
+            $(this).addClass("fbmdcss");
+        });
+        $(this).data({text: text, html: $(this).html(), currFormat: "html"});
+        $(this).addClass("fbmarkdown");
+    });
+}
+
+$(document).on("dblclick", ".fbmarkdown", function(e) {
+    var text = $(this).data("text");
+
+    if ($(this).data("currFormat") == "html") {
+        this.innerHTML = text;
+        $(this).data("currFormat", "text");
+    }
+    else {
+        this.innerHTML = $(this).data("html");
+        $(this).data("currFormat", "html");
+    }
+});
+
+window.addEventListener("scroll",  waitForPause(300, function(e) {
+        if ($(e.target).is(document)) {
+            unfoldUpdates();
+            handleElements(".userContent, .UFICommentBody");
+        }
+        else {
+            setTimeout(function() {
+                handleElements("._kso");
+            }, 300);
+        }
+}), true);
---
+++ readme.md
@@ -0,0 +1,43 @@
+fbmarkdown - Facebook Markdown
+------------------------------
+This is a Google Chrome extension that enables the usage of [Markdown][md] syntax on Facebook.
+
+Newlines are preserved to avoid disrupting the formatting of other peoples' status updates.
+
+[md]: http://www.daringfireball.com/projects/markdown/
+
+# Screenshots
+When writing the update:
+
+![Before]()
+
+And after it's been sent and parsed:
+
+![After]()
+
+It's possible to toggle between source and html by doubleclicking it:
+
+![Source]()
+
+# Usage
+Just install the extension to start using it.
+Updates, comments, and chat messages are parsed automatically.
+
+When entering code, by indenting it 4 spaces, it's also highlighted for you after posting.
+
+# Download
+
+Besides pulling it from the repo, you can download it from my external server:
+
+[`fbmarkdown.crx`](http://rtgkom.dk/~andreashf11/fbmarkdown.crx)
+
+# Thanks
+* John Gruber for Markdown.
+* StackOverflow for the [pagedown](http://code.google.com/p/pagedown/) converter.
+* The jQuery Foundation for [jQuery].
+* [Highlight.js](http://softwaremaniacs.org/soft/highlight/en/).
+* Dustin Curtis for [the icon](https://github.com/dcurtis/markdown-mark).
+
+# License
+Copyright © 2013 Andreas From
+Licensed under GNU General Public License version 3.
